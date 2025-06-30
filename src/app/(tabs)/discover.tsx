e988785fb7ae03d5f0a4ad11d6cd9e1d64339438
ficho-here <ficho.here@gmail.com>
2025-06-30 12:31:50 +0200

    Tabs, ThemeProvider,

---
+++ src/app/(tabs)/discover.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Discover = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>Discover</Text>
+    </View>
+  )
+}
+
+export default Discover
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,22 @@
 import { Slot } from "expo-router";
+import '../../global.css'
+import {DarkTheme, ThemeProvider} from "@react-navigation/native";
+
+const theme = { 
+    ...DarkTheme, 
+    colors: {
+        ...DarkTheme.colors,
+        background: '#010D1A',
+        card: '#010D1A',
+        primary: 'white',
+    }
+}
+
 
 export default function RootLayout() {
     return(
-        <Slot/>
+        <ThemeProvider value={theme}>
+            <Slot/>
+        </ThemeProvider>
     ) 
 }
